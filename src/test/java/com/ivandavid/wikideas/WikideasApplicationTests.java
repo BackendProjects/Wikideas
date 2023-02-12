33f611cb1976f576ce9a25b87a03abf18ee44e6f
ivandavid858 <ivandavid858@gmail.com>
2023-02-12 21:45:58 +0100

    Initial commit

---
+++ src/test/java/com/ivandavid/wikideas/WikideasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandavid.wikideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikideasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
